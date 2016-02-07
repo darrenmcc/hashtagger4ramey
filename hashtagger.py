24b0f0d80e2e2edfef8762bb7d6afaffbea0a12f
Darren McCleary <darren.rmc@gmail.com>
2016-02-06 21:34:23 -0500

    replaced text file with OS dictionary, some cleanup

--- hashtagger.py
+++ hashtagger.py
@@ -2,16 +2,13 @@ import sys
 import random
 
 def main(n):
-    with open('dictionary.txt', 'r') as f:
+    with open('/usr/share/dict/words', 'r') as f:
         hashtags = map(
-            ht_format, 
+            lambda word: '#' + word.strip(), 
             random.sample(f.readlines(), n)
         )
 
-    print ', '.join(hashtags)
-
-def ht_format(word):
-    return '#' + word.strip()
+    print ' '.join(hashtags)
 
 if __name__ == '__main__':
     n = int(sys.argv[1])
